ded316368e735d952c143dba55fb4bc716110b59
khanoftexas <khanoftexas@gmail.com>
2019-05-15 21:50:23 -0500

    Final Load

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,47 @@
 // from data.js
-var tableData = data;
+var ufoData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(tbody)
+data.forEach((ufoSighting) => {
+
+    var row = tbody.append("tr");
+  
+    Object.values(ufoSighting).forEach((value) => {
+  
+      row.append("td").text(value);
+  
+    });
+});
+
+  // Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clear all data
+    d3.select("tbody").html("");
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    
+    var filteredData = ufoData.filter(ufoData => ufoData.datetime === inputValue);
+
+    filteredData.forEach((datedata) => {
+
+        var row = tbody.append("tr");
+        
+        Object.values(datedata).forEach((value) => {
+
+            row.append("td").text(value);
+            
+        })
+    });
+});
+  
